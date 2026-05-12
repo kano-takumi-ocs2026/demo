986f7df5535369fbd04c178a270717e3443bb386
kano-takumi-ocs2026 <253106@ocsjoho.onmicrosoft.com>
2026-05-12 14:57:26 +0900

    変更後

--- src/main/java/com/example/demo/chat/ChatController.java
+++ src/main/java/com/example/demo/chat/ChatController.java
@@ -1,7 +1,8 @@
-package com.example.demo;
+package com.example.demo.chat;
 
 import org.springframework.web.bind.annotation.*;
 
+import com.example.demo.chat.message.ChatMessage;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,17 +11,18 @@ import java.util.List;
 @CrossOrigin
 public class ChatController {
 
-    private final List<String> messages = new ArrayList<>();
+    private final List<ChatMessage> messages = new ArrayList<>();
 
     // メッセージ送信
     @PostMapping
     public void sendMessage(@RequestBody String message) {
-        messages.add(message);
+        ChatMessage chatMessage = new ChatMessage(message, 1L, 1L, 1L);
+        messages.add(chatMessage);
     }
 
     // メッセージ取得
     @GetMapping
-    public List<String> getMessages() {
+    public List<ChatMessage> getMessages() {
         return messages;
     }
 }
